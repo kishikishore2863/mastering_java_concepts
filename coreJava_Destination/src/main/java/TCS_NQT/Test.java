c822c5b4a6256759949c7893967d1fd279002fa9
kishikishore2863 <kishikishore.2863@gmail.com>
2025-03-15 23:56:31 +0530

    merge sort

--- coreJava_Destination/src/main/java/TCS_NQT/Test.java
+++ coreJava_Destination/src/main/java/TCS_NQT/Test.java
@@ -4,20 +4,7 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        int copy = 12521;
-        int n = copy;
-        int reversed=0;
 
-       while (n>0){
-           int digit = n%10;
-           reversed =reversed*10+digit;
-           n=n/10;
-       }
-       if(n == reversed){
-           System.out.println("palindrom");
-       }else {
-           System.out.println("not");
-       }
 
     }
 }
